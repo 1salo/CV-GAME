0729c9480f4b103898a1155069b77208ffb0fa1d
1salo <william-salo@hotmail.com>
2024-03-13 13:11:30 +0100

    done

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Detta är min Dator. Jag jobbar mest i JavaScript/TypeScript och Ruby on rails nu för tiden.
+  Oavsett språk, så gillar jag programmering. Här är min <a href="https://github.com/1salo" target="_blank">Github</a>!`,
+  cv: `Nämen titta! Här hänger mitt CV. <a href="https://github.com/1salo/CV/blob/main/William-Salomonsson%20CV.pdf" target="_blank">Kika in?</a>
+  Kontakta mig på william-salo@hotmail.com om du har några intressanta jobbmöjligheter`,
+  tv: `Där är min TV. Jag kollar mest fotboll. COYS!!`,
+  bed: `Här sover jag. Bra idéer kommer oftast när jag ligger i min säng. När en idé slår mig, måste jag ofta skriva ner den, annars kommer jag inte kunna sova eftersom min hjärna är uppslukad av den.`,
+  library: `Här står det många böcker av olika slag. Min favoritbok just nu är Kafka på Stranden skriven av Haruki Murakami`,
+  dog: `VOFF VOFF - Här är min bästa vän, Milo. Han är en Cavapoo och världens gulligaste hund.`,
+  "sofa-table": `Här står min soffa. Jag gillar att koppla av här och titta på TV eller kanske spela lite playstation`,
+  plants: `Jag gillar blommor och växter`,
+  kitchen: `Jag gillar att laga mat. Oftast någon god pasta`,
+  dogbed: `Här brukar lilla Milo sova`,
+  coys: `Oh when the Spurs, go marching in
+  I wanna be in that number
+  Oh when the Spurs go marching in `,
+  music: `Här står min lp-spelare. Jag älskar lyssna på musik. Speciellt The Beatles`,
+  exit: `Om du vill lämna William Salomonssons CV-spel så är det bara att stänga ner fliken.`,
+};
